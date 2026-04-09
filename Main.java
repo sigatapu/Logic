395ba7651f800c419ccf0f6302e466742042b850
Sigatapu <dattakrishna282@gmail.com>
2026-04-09 21:14:07 +0530

    added four folders and some code

---
+++ Main.java
@@ -0,0 +1,7 @@
+import basic.*;
+class Main{
+    public static void main(String[] args){
+        System.out.println("hello world");
+        Basic.even_or_odd(2);
+    }
+}
---
+++ basic/Basic.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class Basic {
+    public static void  even_or_odd(int n){
+        if(n%2==0) System.out.println("even");
+        else System.out.println("odd");
+    }
+}
