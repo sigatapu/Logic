c2cf93eab53bca5bf83fc670b74b7374cd951e6e
Sigatapu <dattakrishna282@gmail.com>
2026-04-09 21:49:40 +0530

    did some work

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ import basic.*;
 class Main{
     public static void main(String[] args){
         System.out.println("hello world");
-        Basic.even_or_odd(2);
+        // Basic.even_or_odd(2);
+        // Basic.multiplication_table(2);
+        System.out.println(Basic.sum_of_squares(5));
     }
 }
--- basic/Basic.java
+++ basic/Basic.java
@@ -1,8 +1,39 @@
 package basic;
 
 public class Basic {
+
     public static void  even_or_odd(int n){
         if(n%2==0) System.out.println("even");
         else System.out.println("odd");
     }
+    
+    public static void multiplication_table(int n){
+        for(int i=1 ;i<=10 ; i++){
+            System.out.println(n+" X "+i + " = "+n*i);
+        }
+    }
+
+    public static int sum_of_natural_numbers(int n){
+        int result = 0 ;
+        for (int i=1; i<=n; i++){
+            result = result + i;
+        }
+        return result ;
+    }
+
+    public static int sum_of_squares(int n){
+        int result = 0 ;
+        for(int i=1 ; i<=n ; i++){
+            result = result + (i*i);
+        }
+        return result;
+    }
+
+    public static void swap(int a , int b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    
 }
